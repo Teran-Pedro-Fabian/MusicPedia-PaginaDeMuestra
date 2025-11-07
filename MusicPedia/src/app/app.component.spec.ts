f7389a1bb20c76717c252cbc5a13dfc79b34d3a0
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2025-11-07 00:12:22 -0300

    inicio de la pagina de musica

--- MusicPedia/src/app/app.component.spec.ts
+++ MusicPedia/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MusicPedria' title`, () => {
+  it(`should have the 'MusicPedia' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MusicPedria');
+    expect(app.title).toEqual('MusicPedia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MusicPedria');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MusicPedia');
   });
 });
